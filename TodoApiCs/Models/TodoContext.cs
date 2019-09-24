4935e518654e5b020a14f9a7ffe1ad40853834e2
Luke Fiji <luke.fiji@gmail.com>
2019-09-24 12:46:51 -0700

    Add & register in-memory db context, add controller & get methods

---
+++ TodoApiCs/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+// The database context is the main class that coordinates Entity Framework functionality for a data model
+// This class is created by deriving from the Microsoft.EntityFrameworkCore.DbContext class
+namespace TodoApiCs.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
--- TodoApiCs/Models/TodoItem.cs
+++ TodoApiCs/Models/TodoItem.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// A model is a set of classes that represent the data that the app manages
 namespace TodoApiCs.Models
 {
+    // The model for this app is a single TodoItem class
     public class TodoItem
     {
-        public long Id { get; set; }
+        public long Id { get; set; } // Database key
         public string Name { get; set; }
         public bool IsComplete { get; set; }
     }
